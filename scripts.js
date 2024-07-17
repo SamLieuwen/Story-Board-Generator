8da18f40ddc1393fbff5b45e2863026d600f6db1
SamLieuwen <samlieuwen12@gmail.com>
2024-07-17 13:51:30 -0500

    finished

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
+let i = 0;
+
 function append(e)
 {
     e.preventDefault();
+
+    i += 1;
     
     const storyBoard = document.getElementById("storyBoard");
-    const story = document.createElement("div");
+
+    let story = document.createElement("div");
+    story.setAttribute("id", `story-${i}`);
     
     let asa = document.getElementById("asa").value;
     let iwant = document.getElementById("iwant").value;
     let sothat = document.getElementById("sothat").value;
-    let text = document.createTextNode("As a " + asa + ", I want " + iwant + ", so that" + sothat);
+    let acceptanceCriteria = document.getElementById("acceptancecriteria").value;
+    let notes = document.getElementById("notes").value;
+
+    story.innerHTML = "<p>As a " + asa + ", I want " + iwant + ", so that " + sothat + ".</p>";
+    story.innerHTML += "<p>Criteria: <br>" + acceptanceCriteria + "</p>";
+    story.innerHTML += "<p>Notes: <br>" + notes + "</p>";
+    story.innerHTML += `<input type="button" value="Copy" onclick="copy('${story.innerText}')"> `
     
-    story.appendChild(text);
     storyBoard.prepend(story);
 }
 
+function copy(story)
+{
+    navigator.clipboard.writeText(story);
+}
