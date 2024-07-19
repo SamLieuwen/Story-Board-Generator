3ba3bf1b684d1d8833635e2f41aeb5f023d1365b
SamLieuwen <samlieuwen12@gmail.com>
2024-07-19 13:46:19 -0500

    problems with submit button

--- scripts.js
+++ scripts.js
@@ -1,26 +1,49 @@
+const storyBoard = document.getElementById("storyBoard");
 let i = 0;
+let hasChanged;
+let story;
+let asa;
+let iwant;
+let sothat;
+let acceptanceCriteria;
+let notes;
+let tempStory = "As a " + asa + ", I want " + iwant + ", so that " + sothat + ".\n\nCriteria: \n" + acceptanceCriteria + "\n\nNotes: \n" + notes + "\n\n";
 
 function append(e)
 {
     e.preventDefault();
 
     i += 1;
+    hasChanged = true;
     
-    const storyBoard = document.getElementById("storyBoard");
-
-    let story = document.createElement("div");
+    story = document.createElement("div");
     story.setAttribute("id", `story-${i}`);
     
-    let asa = document.getElementById("asa").value;
-    let iwant = document.getElementById("iwant").value;
-    let sothat = document.getElementById("sothat").value;
-    let acceptanceCriteria = document.getElementById("acceptancecriteria").value;
-    let notes = document.getElementById("notes").value;
+    asa = document.getElementById("asa").value;
+    iwant = document.getElementById("iwant").value;
+    sothat = document.getElementById("sothat").value;
+    acceptanceCriteria = document.getElementById("acceptancecriteria").value;
+    notes = document.getElementById("notes").value;
 
     story.innerText = "As a " + asa + ", I want " + iwant + ", so that " + sothat + ".\n\nCriteria: \n" + acceptanceCriteria + "\n\nNotes: \n" + notes + "\n\n";
     story.innerHTML += `<input type="button" value="Copy" onclick="copy('${story.innerHTML}')"> `
-
-    storyBoard.prepend(story);
+    
+    for (let j = 1; j < i + 1; j++)
+    {   
+        if (tempStory != document.getElementById(`story-${j}`).innerText)
+        { hasChanged = true; }
+        else { hasChanged = false; break; }
+    }
+    
+    if (hasChanged == true)
+    {
+        tempStory = story.innerText;
+        storyBoard.prepend(story);
+    }
+    else
+    {
+        alert("Data has not changed or already exists");
+    }
 }
 
 function copy(story)
