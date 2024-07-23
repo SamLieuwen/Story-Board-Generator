5b067a614d27968dc37384eec176fe245bf401de
SamLieuwen <samlieuwen12@gmail.com>
2024-07-23 10:05:31 -0500

    first iteration

--- scripts.js
+++ scripts.js
@@ -1,48 +1,40 @@
-const storyBoard = document.getElementById("storyBoard");
 let i = 0;
 let hasChanged;
-let story;
-let asa;
-let iwant;
-let sothat;
-let acceptanceCriteria;
-let notes;
-let tempStory = "As a " + asa + ", I want " + iwant + ", so that " + sothat + ".\n\nCriteria: \n" + acceptanceCriteria + "\n\nNotes: \n" + notes + "\n\n";
+
+const checkDefault = document.createElement("div");
+checkDefault.setAttribute("id", `story-${0}`);
+checkDefault.innerText += "As a , I want , so that .\n\nCriteria: \n\n\nNotes: \n\n\n";
 
 function append(e)
 {
     e.preventDefault();
 
     i += 1;
-    hasChanged = true;
+    hasChanged = false;
     
-    story = document.createElement("div");
+    const storyBoard = document.getElementById("storyBoard");
+    let story = document.createElement("div");
     story.setAttribute("id", `story-${i}`);
     
-    asa = document.getElementById("asa").value;
-    iwant = document.getElementById("iwant").value;
-    sothat = document.getElementById("sothat").value;
-    acceptanceCriteria = document.getElementById("acceptancecriteria").value;
-    notes = document.getElementById("notes").value;
+    let asa = document.getElementById("asa").value;
+    let iwant = document.getElementById("iwant").value;
+    let sothat = document.getElementById("sothat").value;
+    let acceptanceCriteria = document.getElementById("acceptancecriteria").value;
+    let notes = document.getElementById("notes").value;
 
     story.innerText = "As a " + asa + ", I want " + iwant + ", so that " + sothat + ".\n\nCriteria: \n" + acceptanceCriteria + "\n\nNotes: \n" + notes + "\n\n";
     story.innerHTML += `<input type="button" value="Copy" onclick="copy('${story.innerHTML}')"> `
-    
-    for (let j = 1; j < i + 1; j++)
-    {   
-        if (tempStory != document.getElementById(`story-${j}`).innerText)
-        { hasChanged = true; }
-        else { hasChanged = false; break; }
-    }
+
+    checkChange(story.innerHTML);
     
     if (hasChanged == true)
     {
-        tempStory = story.innerText;
-        storyBoard.prepend(story);
+        storyBoard.prepend(story);   
     }
     else
     {
-        alert("Data has not changed or already exists");
+        alert("Data hasn't changed or already exists");
+        i -= 1;
     }
 }
 
@@ -55,4 +47,31 @@ function copy(story)
     temp.remove();
 
     navigator.clipboard.writeText(text);
+}
+
+function checkChange(story)
+{
+    const temp = document.createElement("div");
+    temp.setAttribute("id", `story-${i}`);
+    temp.innerHTML += story;
+    document.body.append(temp);
+    const current = temp.innerText;
+
+    document.body.append(checkDefault);
+
+    console.log(current);
+
+    for (let j = i - 1; j >= 0; j--)
+    {   
+        const previousStory = document.getElementById(`story-${j}`).innerText;
+        
+        console.log(previousStory + j);
+
+        if (current != previousStory)
+        { hasChanged = true; }
+        else { break; }
+    }
+
+    temp.remove();
+    checkDefault.remove();
 }
