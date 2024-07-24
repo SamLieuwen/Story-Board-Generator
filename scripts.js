a27e85bd0144deefe6d40f235efe052b8bc0c0f7
SamLieuwen <samlieuwen12@gmail.com>
2024-07-24 08:11:21 -0500

    Auto expanding input fields

--- scripts.js
+++ scripts.js
@@ -86,3 +86,8 @@ function clearTextArea(e)
 
     document.getElementById("form").reset();
 }
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+  }
